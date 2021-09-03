ed44b9b7b2486d3f29e580521dfe65dbb95e905c
NicolaMascarenhas <nicolamas.2013@gmail.com>
2021-09-04 00:06:30 +0530

    Reducing slug size

--- gen_heatmap.py
+++ gen_heatmap.py
@@ -22,29 +22,24 @@ def generate_heatmap(img, modelPath, last_conv_layer_name, last_layer_name,):
     return save_and_display_gradcam(img, heatmap)
 
 def make_gradcam_heatmap(img_array, model, last_conv_layer_name, last_layer_name, pred_index=None):
-    # First, we create a model that maps the input image to the activations
-    # of the last conv layer as well as the output predictions
+    # First, we create a model that maps the input image to the activations of the last conv layer as well as the output predictions
 
     grad_model = tf.keras.models.Model([model.inputs], [model.get_layer(last_conv_layer_name).output, model.get_layer(last_layer_name).output])
 
-    # Then, we compute the gradient of the top predicted class for our input image
-    # with respect to the activations of the last conv layer
+    # Then, we compute the gradient of the top predicted class for our input image with respect to the activations of the last conv layer
     with tf.GradientTape() as tape:
         last_conv_layer_output, preds = grad_model(img_array)
         if pred_index is None:
             pred_index = tf.argmax(preds[0])
         class_channel = preds[:, pred_index]
 
-    # This is the gradient of the output neuron (top predicted or chosen)
-    # with regard to the output feature map of the last conv layer
+    # This is the gradient of the output neuron (top predicted or chosen) with regard to the output feature map of the last conv layer
     grads = tape.gradient(class_channel, last_conv_layer_output)
 
-    # This is a vector where each entry is the mean intensity of the gradient
-    # over a specific feature map channel
+    # This is a vector where each entry is the mean intensity of the gradient over a specific feature map channel
     pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
 
-    # We multiply each channel in the feature map array
-    # by "how important this channel is" with regard to the top predicted class
+    # We multiply each channel in the feature map array by "how important this channel is" with regard to the top predicted class
     # then sum all the channels to obtain the heatmap class activation
     last_conv_layer_output = last_conv_layer_output[0]
     heatmap = last_conv_layer_output @ pooled_grads[..., tf.newaxis]
